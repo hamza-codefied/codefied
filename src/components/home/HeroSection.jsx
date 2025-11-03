73d5e9f0df70d4738603c2252776ffdda82c607a
hamza-codefied <hamza.codefied123@gmail.com>
2025-11-03 08:15:28 -0600

    resolving images issues

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -2,13 +2,13 @@ import { Button } from 'antd';
 import React from 'react';
 import Section from '@layouts/Section';
 import { TypeAnimation } from 'react-type-animation';
-import hero_bg from '@images/hero_bg.png';
+import herobg from '@images/hero_bg.png';
 
 export default function HeroSection() {
   return (
     <Section
       classNames='relative bg-cover bg-center bg-no-repeat py-10 sm:py-[120px]'
-      style={{ backgroundImage: `url(${hero_bg})` }}
+      style={{ backgroundImage: `url(${herobg})` }}
     >
       <div className='relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='text-center'>
