c5e12b5a4f702fcf6ada779d8faa5ec5ccfd5941
hamza-codefied <hamza.codefied123@gmail.com>
2025-11-24 09:57:29 -0600

    blog detail and portfolio detail page

--- src/components/AboutUs/MapSection.jsx
+++ src/components/AboutUs/MapSection.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import map from '@/images/map.png';
+import map from '@/images/map2.png';
 
 const MapSection = () => {
   return (
     <section className='relative'>
       {/* Map Image */}
-      <div className='max-w-full flex items-center justify-center mx-auto'>
+      <div className='max-w-5xl flex items-center justify-center mx-auto'>
         <img
           src={map}
           alt='Global Office Map'
