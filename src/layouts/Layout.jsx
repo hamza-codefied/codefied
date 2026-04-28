a773caa41fac66765689b4bba53c6a0d35ce6e07
MuhammadAliGabol <aligabol2002@gmail.com>
2026-04-28 21:49:59 +0500

    Add a development notice banner to the layout component

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -4,6 +4,9 @@ import { Footer } from '@components/layout/Footer'
 export const Layout = ({ children }) => {
   return (
     <div className="min-h-screen flex flex-col max-w-full">
+      <div className="w-full bg-yellow-300 text-black text-center text-sm sm:text-base font-semibold py-2 px-4">
+        Website is under development.
+      </div>
       <Header />
       <main className="flex-1 max-w-full">
         {children}
